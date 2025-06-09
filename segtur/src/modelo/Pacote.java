959184280d0588cd5b083563704b1bd150aa5070
Germano Pinheiro <germanos.pinheiro@gmail.com>
2025-06-09 00:38:46 -0300

    Finalizado criacao de todas classes DAO basicas.
    Criado todas entidades basicas.

---
+++ segtur/src/modelo/Pacote.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import java.util.Date;
+
+public class Pacote {
+    private int idPacote;
+    private String nome;
+    private Date dataChegada;
+    private Date dataSaida;
+    private String itineario;
+    private double preco;
+    private int vagas;
+
+}
